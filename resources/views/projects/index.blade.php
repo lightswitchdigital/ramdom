3e6c0918c52839a4b4578fde3ad26d4c04d54c5e
sireax <sireax@yandex.ru>
2020-11-26 17:45:08 +0400

    conflict resolved

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -16,35 +16,4 @@
             @endforeach
         </section>
 
-<<<<<<< HEAD
-                                @elseif ($attribute->isNumber())
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <input type="number" class="form-control" name="attrs[{{ $attribute->id }}][from]" value="{{ request()->input('attrs.' . $attribute->id . '.from') }}" placeholder="From">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <input type="number" class="form-control" name="attrs[{{ $attribute->id }}][to]" value="{{ request()->input('attrs.' . $attribute->id . '.to') }}" placeholder="To">
-                                        </div>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            </div>
-
-            <button class="btn btn-success">Искать</button>
-        </form>
-        <div class="flex-box">
-        @foreach($projects as $project)
-            <project-card
-                :project="{{ $project }}"
-                :project-link="'{{ route('projects.show', $project) }}'"
-            ></project-card>
-        @endforeach
-        </div>
-=======
->>>>>>> ff8e82f1228794102b035ad5950001d54c1bb0cf
-    </div>
-
 @endsection
