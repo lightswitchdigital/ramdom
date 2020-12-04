ac5ca0c62500e8bc4527209c0ffc7b2bf7a2a9d9
Matveyx25 <matveyx25@gmail.com>
2020-12-04 15:53:49 +0400

    Profile page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,27 +8,38 @@
             <div class="col-md-4">
                 <ul class="menu">
                     <li>
-                        <a href="#">Мои проекты</a>
+                        <a href="#"><img src="/images/presets/shopping-bag.png"> Мои проекты</a>
                     </li>
                     <li>
-                        <a href="#">Понравившиеся проекты</a>
+                        <a href="#"><img src="/images/presets/heart.png">Понравившиеся проекты</a>
                     </li>
                     <li>
-                        <a href="#">Строительство</a>
+                        <a href="#"><img src="/images/presets/technics.png">Строительство</a>
                     </li>
                     <li>
-                        <a href="#">Акции и скидки</a>
+                        <a href="#"><img src="/images/presets/price-tag.png">Акции и скидки</a>
                     </li>
                     <li>
-                        <a href="#">Настройки</a>
+                        <a href="#"><img src="/images/presets/settings.png">Настройки</a>
                     </li>
                     <li>
-                        <a href="#">Выйти</a>
+                        <a href="#"><img src="/images/presets/logout.png">Выйти</a>
                     </li>
                 </ul>
             </div>
             <div class="col">
-                
+                <div class="profile-info">
+                    <div class="img">
+                     <img src="/images/boss.png">   
+                    </div>
+                    <div class="info">
+                        <h1>Имя</h1>
+                        <h1>Фамилия</h1>
+                        <p>www.ctroitel.com</p>
+                        <p>г. Самара ул. Ново-Садовая 12</p>
+                        <p>8 846 123 45 67</p>
+                    </div>
+                </div>
             </div>
         </div>    
     </div>
