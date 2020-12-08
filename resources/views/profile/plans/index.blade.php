3fcb5f9c72aaf911954acc3a1f46fd15a3af1d96
Matveyx25 <matveyx25@gmail.com>
2020-12-08 17:02:14 +0400

    Plans cards

--- resources/views/profile/plans/index.blade.php
+++ resources/views/profile/plans/index.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 @foreach($plans as $plan)
                     <div class="col-md">
-                        <a href="{{ route(`profiile.plans.subscribe`, $plan) }}" class="card">
+                        <a href="{{ route('profiile.plans.subscribe', $plan) }}" class="card">
                             <h4 class="card-title">{{ $plan->name }}</h4>
                             <h1 class="price">{{ $plan->price }}</h1>
                         </a>
