767b237b94833ce201b69a86e7c9821bd1ac3088
Matveyx25 <matveyx25@gmail.com>
2020-12-17 12:19:14 +0400

    Dropdowns fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,7 @@
                             </li>
 
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle user-btn" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
