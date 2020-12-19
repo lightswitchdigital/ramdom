2e06058128f13a3df5d3ccdfde1d71d5fc2e0859
sireax <sireax@yandex.ru>
2020-12-19 14:17:33 +0400

    patch to previous commit

--- resources/views/profile/settings/index.blade.php
+++ resources/views/profile/settings/index.blade.php
@@ -47,92 +47,96 @@
 
         <hr>
 
-        <form action="{{ route('profile.settings.update-individual-information') }}" method="POST" class="my-3">
-            @csrf
-            @method('PUT')
+        @if($user->isIndividual())
 
-            <div class="form-group">
-                <label for="passport_serial" class="col-form-label">Серия паспорта</label>
-                <input id="passport_serial" class="form-control @error('passport_serial') is-invalid @enderror" name="passport_serial" value="{{ old('passport_serial', $user->passport_serial) }}">
-                @error('passport_serial')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+            <form action="{{ route('profile.settings.update-individual-information') }}" method="POST" class="my-3">
+                @csrf
+                @method('PUT')
 
-            <div class="form-group">
-                <label for="passport_code" class="col-form-label">Номер паспорта</label>
-                <input id="passport_code" class="form-control @error('passport_code') is-invalid @enderror" name="passport_code" value="{{ old('passport_code', $user->passport_code) }}">
-                @error('passport_code')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="passport_serial" class="col-form-label">Серия паспорта</label>
+                    <input id="passport_serial" class="form-control @error('passport_serial') is-invalid @enderror" name="passport_serial" value="{{ old('passport_serial', $user->passport_serial) }}">
+                    @error('passport_serial')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <label for="passport_issue" class="col-form-label">Кем выдан</label>
-                <input id="passport_issue" class="form-control @error('passport_issue') is-invalid @enderror" name="passport_issue" value="{{ old('passport_issue', $user->passport_issue ) }}">
-                @error('passport_issue')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="passport_code" class="col-form-label">Номер паспорта</label>
+                    <input id="passport_code" class="form-control @error('passport_code') is-invalid @enderror" name="passport_code" value="{{ old('passport_code', $user->passport_code) }}">
+                    @error('passport_code')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <label for="passport_issue_date" class="col-form-label">Дата выдачи</label>
-                <input id="passport_issue_date" class="form-control @error('passport_issue_date') is-invalid @enderror" name="passport_issue_date" value="{{ old('passport_issue_date', $user->passport_issue_date? $user->passport_issue_date->format('d.m.Y') : '') }}">
-                @error('passport_issue_date')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="passport_issue" class="col-form-label">Кем выдан</label>
+                    <input id="passport_issue" class="form-control @error('passport_issue') is-invalid @enderror" name="passport_issue" value="{{ old('passport_issue', $user->passport_issue ) }}">
+                    @error('passport_issue')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <button class="btn btn-primary">
-                    Обновить
-                </button>
-            </div>
-        </form>
+                <div class="form-group">
+                    <label for="passport_issue_date" class="col-form-label">Дата выдачи</label>
+                    <input id="passport_issue_date" class="form-control @error('passport_issue_date') is-invalid @enderror" name="passport_issue_date" value="{{ old('passport_issue_date', $user->passport_issue_date? $user->passport_issue_date->format('d.m.Y') : '') }}">
+                    @error('passport_issue_date')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-        <hr>
+                <div class="form-group">
+                    <button class="btn btn-primary">
+                        Обновить
+                    </button>
+                </div>
+            </form>
 
-        <form action="{{ route('profile.settings.update-entity-information') }}" method="POST" class="my-3">
-            @csrf
-            @method('PUT')
+        @elseif($user->isEntity())
 
-            <div class="form-group">
-                <label for="company_name" class="col-form-label">Название компании</label>
-                <input id="company_name" class="form-control @error('company_name') is-invalid @enderror" name="company_name" value="{{ old('company_name', $user->company_name) }}">
-                @error('company_name')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+            <form action="{{ route('profile.settings.update-entity-information') }}" method="POST" class="my-3">
+                @csrf
+                @method('PUT')
 
-            <div class="form-group">
-                <label for="company_address" class="col-form-label">Юридический адрес</label>
-                <input id="company_address" class="form-control @error('company_address') is-invalid @enderror" name="company_address" value="{{ old('company_address', $user->company_address) }}">
-                @error('company_address')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="company_name" class="col-form-label">Название компании</label>
+                    <input id="company_name" class="form-control @error('company_name') is-invalid @enderror" name="company_name" value="{{ old('company_name', $user->company_name) }}">
+                    @error('company_name')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <label for="company_inn" class="col-form-label">ИНН</label>
-                <input id="company_inn" class="form-control @error('company_inn') is-invalid @enderror" name="company_inn" value="{{ old('company_inn', $user->company_inn) }}">
-                @error('company_inn')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="company_address" class="col-form-label">Юридический адрес</label>
+                    <input id="company_address" class="form-control @error('company_address') is-invalid @enderror" name="company_address" value="{{ old('company_address', $user->company_address) }}">
+                    @error('company_address')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <label for="company_account" class="col-form-label">Расчетный счет</label>
-                <input id="company_account" class="form-control @error('company_account') is-invalid @enderror" name="company_account" value="{{ old('company_account', $user->company_account) }}">
-                @error('company_account')
-                <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
-                @enderror
-            </div>
+                <div class="form-group">
+                    <label for="company_inn" class="col-form-label">ИНН</label>
+                    <input id="company_inn" class="form-control @error('company_inn') is-invalid @enderror" name="company_inn" value="{{ old('company_inn', $user->company_inn) }}">
+                    @error('company_inn')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <button class="btn btn-primary">
-                    Обновить
-                </button>
-            </div>
-        </form>
+                <div class="form-group">
+                    <label for="company_account" class="col-form-label">Расчетный счет</label>
+                    <input id="company_account" class="form-control @error('company_account') is-invalid @enderror" name="company_account" value="{{ old('company_account', $user->company_account) }}">
+                    @error('company_account')
+                    <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-primary">
+                        Обновить
+                    </button>
+                </div>
+            </form>
+
+        @endif
     </div>
 @endsection
