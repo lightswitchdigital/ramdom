5a7b66a527b239818e3d0acd0537ab162900d0ff
Matveyx25 <matveyx25@gmail.com>
2020-12-28 23:22:27 +0400

    plans

--- resources/views/profile/plans/index.blade.php
+++ resources/views/profile/plans/index.blade.php
@@ -3,20 +3,33 @@
 @section('content')
     <div class="plans-block">
         <div class="container">
-            <h1 class="title">Проекты</h1>
+            <h1 class="title">Планы</h1>
+            <h1 class="subtitle">Выберите подходящий себе план</h1>
             <div class="row">
-                @foreach($plans as $plan)
+                @foreach($plans as $plan) 
                     <div class="col-md">
                         <div class="card">
                             <form action="{{ route('profile.plans.subscribe', $plan) }}" method="POST">
                                 @csrf
                                 <h4 class="card-title">{{ $plan->name }}</h4>
                                 <h1 class="price">{{ $plan->price }}</h1>
-                                <button class="btn btn-primary">Выбрать</button>
+                                <p class="desc">план для тех, кто только начинает знакомиться с функционалом сайта</p>
+                                <button class="btn yellow-btn">Перейти</button>
                             </form>
                         </div>
                     </div>
                 @endforeach
+                <div class="col-md">
+                    <div class="card big-card">
+                        <form>
+                            @csrf
+                            <h4 class="card-title">Профессионал</h4>
+                            <h1 class="price">100000</h1>
+                            <p class="desc">план для тех, кто только начинает знакомиться с функционалом сайта</p>
+                            <button class="btn yellow-btn">Выбрать</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
