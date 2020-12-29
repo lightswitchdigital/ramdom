df9183bf5a47e99e6d423cc7046f25a59075e288
Matveyx25 <matveyx25@gmail.com>
2020-12-29 14:47:46 +0400

    Emit commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,7 +117,15 @@
         </nav>
 
         <main class="py-4 content">
-
+            <div class="container">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Library</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data</li>
+                    </ol>
+                </nav>
+            </div>
             @include('layouts.common.partials')
 
             @yield('content')
